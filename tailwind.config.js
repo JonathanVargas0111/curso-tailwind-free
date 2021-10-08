d6a7d60625c1dd867a43d0d84c77897886803c39
jonathan vargas <jonathanfelipe0111@hotmail.com>
2021-10-07 21:01:49 -0500

    Configuracion inicial de tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+    purge:[
+        './public/**/*.html',
+    ],
+    darMode: false,
+    theme:{
+        extend:{}
+    },
+    variants:{
+        extend:{},
+    },
+    plugins:[],
+
+}
